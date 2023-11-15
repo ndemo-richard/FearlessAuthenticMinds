639071e347f0d994fe500ab824ea07558da8bbe7
ndemo-richard <ndemorich@gmail.com>
2023-11-15 08:32:37 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,23 @@ function Home() {
 <section className="bg-inherit">
     <div className="grid gap-8 max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 sm:gap-8">
         <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-3xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-5xl dark:text-white">Your Fearless Authenticity</h1>
+            <h1 className="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-4xl dark:text-white">FEARLESS AUTHENTIC WELLNESS</h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Nurturing Minds, Transforming Lives, One Authentic Step at a Time</p>
-            <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                Get started
-                <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-            <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Speak to Us
-            </a> 
+           <p className="mb-4 max-w-2xl mb-6 font-light text-slate-900">At Fearless Authentic Wellness Consultancy, we&#39;re dedicated to leveraging the intersection of mental
+health, innovation, and personal empowerment to unlock long-lasting well-being and drive positive
+transformation in individuals and organizations alike
+To achieve this, we aim at providing mental health and psychosocial support services to both individual
+and groups as well as undertake mental health consultancy for organizations. Our focus is on the
+following key areas:</p>
+<ul className="mb-4 max-w-2xl mb-6 font-light text-slate-900">
+<li>❖ Providing Counseling Services to Individuals and Groups</li>
+<li>❖ Conducting Mental Health talks to organizations</li>
+<li>❖ Advocating for mental health and self-awareness</li>
+<li>❖ Providing Consultancy Services on developing mental health programmes &amp; wellness policies</li>
+<li>❖ Advocacy on the right to mental Health.</li>
+</ul>
+
+            
         </div>
         <div className="lg:mt-0 lg:col-span-5 lg:flex rounded-xl bg-inherit">
             <img className="rounded-full"src="/images/two.jpg" alt="mockup" />
@@ -26,7 +34,7 @@ function Home() {
       <section className="mt-5 bg-white dark:bg-gray-900" id='about'>
     <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">FEARLESS AUTHENTIC WELLNESS CONSULTANCY</h2>
+            <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white">FEARLESS AUTHENTIC WELLNESS CONSULTANCY</h2>
             <p className="mb-4">Living in a world that most often exerts pressure on us to ‘’fit in’’ can be overwhelming and many like
 are sucked into believing that they actually need to ‘’fit in’’ to feel like they belong. So, what will happen
 if we actually be OURSELVES instead of trying so hard to ‘’fit in’’ in a world that is constantly trying to
@@ -47,68 +55,47 @@ Embrace Your Authenticity Fearlessly for a Fulfilling life!
 <section className="mt-5 bg-white dark:bg-gray-900" id="service">
   <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
       <div className="max-w-screen-md mb-8 lg:mb-16">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Embrace Your Authenticity: Fearless Wellness for a Fulfilling Life</h2>
-          <p className="text-gray-500 sm:text-xl dark:text-gray-400">At Fearless Authentic Wellness Consultancy, we&apos;re dedicated to leveraging the intersection of mental health, innovation, and personal empowerment to unlock long-lasting well-being and drive positive transformation in individuals and organizations alike</p>
+          <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white">Services</h2>
       </div>
-      <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-          <div>
-              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 18">
-    <path d="M7 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9Zm2 1H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-  </svg>
-              </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">Individual Counseling</h3>
-              <p className="text-gray-500 dark:text-gray-400">Advocating for mental health and self-awareness</p>
-          </div>
-          <div>
-              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 19">
-    <path d="M14.5 0A3.987 3.987 0 0 0 11 2.1a4.977 4.977 0 0 1 3.9 5.858A3.989 3.989 0 0 0 14.5 0ZM9 13h2a4 4 0 0 1 4 4v2H5v-2a4 4 0 0 1 4-4Z"/>
-    <path d="M5 19h10v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2ZM5 7a5.008 5.008 0 0 1 4-4.9 3.988 3.988 0 1 0-3.9 5.859A4.974 4.974 0 0 1 5 7Zm5 3a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm5-1h-.424a5.016 5.016 0 0 1-1.942 2.232A6.007 6.007 0 0 1 17 17h2a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5ZM5.424 9H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h2a6.007 6.007 0 0 1 4.366-5.768A5.016 5.016 0 0 1 5.424 9Z"/>
-  </svg>
-              </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">Group counseling</h3>
-              <p className="text-gray-500 dark:text-gray-400">Coaching and Mentorship</p>
-          </div>
-          <div>
-              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-    <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-  </svg>                    
-              </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">Couples counseling and Family therapy</h3>
-              <p className="text-gray-500 dark:text-gray-400">Customized Mental Health and Wellness Training</p>
-          </div>
-          <div>
-              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 18">
-    <path d="M7 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9Zm2 1H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-  </svg>
-              </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">Child and Adolescents counseling</h3>
-              <p className="text-gray-500 dark:text-gray-400">Advocacy on the right to mental Health.</p>
-          </div>
-          <div>
-              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-    <path d="M11.074 4 8.442.408A.95.95 0 0 0 7.014.254L2.926 4h8.148ZM9 13v-1a4 4 0 0 1 4-4h6V6a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v13a1 1 0 0 0 1 1h17a1 1 0 0 0 1-1v-2h-6a4 4 0 0 1-4-4Z"/>
-    <path d="M19 10h-6a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1Zm-4.5 3.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM12.62 4h2.78L12.539.41a1.086 1.086 0 1 0-1.7 1.352L12.62 4Z"/>
-  </svg>
-              </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">Corporate Mental Health Services</h3>
-              <p className="text-gray-500 dark:text-gray-400">Providing Consultancy Services on developing mental health programmes & wellness policies</p>
-          </div>
-          <div>
-              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-    <path d="M11.074 4 8.442.408A.95.95 0 0 0 7.014.254L2.926 4h8.148ZM9 13v-1a4 4 0 0 1 4-4h6V6a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v13a1 1 0 0 0 1 1h17a1 1 0 0 0 1-1v-2h-6a4 4 0 0 1-4-4Z"/>
-    <path d="M19 10h-6a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1Zm-4.5 3.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM12.62 4h2.78L12.539.41a1.086 1.086 0 1 0-1.7 1.352L12.62 4Z"/>
-  </svg>
-              </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">Organizational Change & Growth</h3>
-              <p className="text-gray-500 dark:text-gray-400">Conducting Mental Health talks to organizations</p>
-          </div>
-      </div>
+      <ul className="mb-4 max-w-2xl mb-6 font-light text-slate-900">
+          <li>❖Individual Counseling</li>
+          <li> ❖ Couples counseling and Family therapy</li>
+          <li>❖ Group counseling</li>
+          <li>❖ Child and Adolescents counseling</li>
+          <li> ❖ Personal growth and development</li>
+          <li>❖ Coaching and Mentorship</li>
+        </ul>
+        
+        <h2 className="mb-4 text-2xl tracking-tight font-extrabold text-gray-700 dark:text-white">Corporate Mental Health Wellness</h2>
+<p className='mb-4'>Enhances the strengthening of internal controls on Occupation related risks such as; work stress,
+emotional exhaustion, burnout among others. Therefore, it is vital to create and enabling environment
+through which employees are able to safely express and share experiences encountered in the course of
+work. Mental health wellness in an organization further empowers employees with basic counseling and
+coping skills for safe engagement with complainants and avoid vicarious trauma.</p>
+
+<ul>
+<li>Customized Mental Health and Wellness Training
+<ul>
+<li>Capacity Building</li>
+<li>• Work life balance</li>
+<li>• Conflict resolution in the workplace</li>
+<li>• Emotional intelligence</li>
+</ul></li>
+<li>Debriefing
+<ul>
+<li>• Mental Health Talks</li>
+<li>• Motivational Talks</li>
+<li>• Personal growth and development at the workplace</li>
+</ul></li>
+<li>Workplace counselling</li>
+<li>Organizational Change &amp; Growth</li>
+</ul>
+
+
+        
+     
+    
+    
   </div>
 </section>
 
